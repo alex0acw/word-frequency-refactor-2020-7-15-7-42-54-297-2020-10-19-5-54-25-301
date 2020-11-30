e2e4194e4a81052b4022ac8f0ac027f9a4e02a40
acw <alex_acw@outlook.com>
2020-11-30 17:05:26 +0800

    [WordFrequency][Alex Au] Refactor: Reformat code

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,20 +1,15 @@
 public class Input {
-    private String value;
-    private int count;
+    private final String value;
+    private final int count;
 
-    public Input(String w, int i){
-        this.value =w;
-        this.count =i;
+    public Input(String w, int i) {
+        this.value = w;
+        this.count = i;
     }
-
-
     public String getValue() {
         return this.value;
     }
-
     public int getWordCount() {
         return this.count;
     }
-
-
 }
